d5df99487dcf1abc4859f62ee9aa991bb3b71e3c
Louise Dagher <louise.dagher@cri-paris.org>
2015-11-20 16:15:51 +0100

    premier excercice

---
+++ exercises/085/solution.py
@@ -0,0 +1,11 @@
+def sort_a_list(a) :
+    OK = False
+    while OK == False:
+        OK = True
+        for i in range(0, len(a) - 1):
+            if a[i] < a[i + 1]:             
+                tmp = a[i]
+                a[i] = a[i + 1]
+                a[i + 1] = tmp
+                OK = False
+    return(a)
