97a9bef35381a44294202afb186cfa16691dac74
Louise Dagher <louise.dagher@cri-paris.org>
2015-11-20 16:36:43 +0100

    améliorationexercice1

--- exercises/085/solution.py
+++ exercises/085/solution.py
@@ -1,6 +1,6 @@
-def sort_a_list(a) :
+def sort_a_list(a):
     OK = False
-    while OK is False :
+    while OK is False:
         OK = True
         for i in range(0, len(a) - 1):
             if a[i] < a[i + 1]:
