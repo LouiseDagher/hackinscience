d6f15a783023704b7cb9df5f77c0104c9bd87f8c
Louise Dagher <louise.dagher@cri-paris.org>
2015-12-04 16:04:42 +0100

    Alice et Silvester

--- exercises/097/solution.py
+++ exercises/097/solution.py
@@ -3,3 +3,14 @@ def love_meet(alice, bob):
     b = set(bob)
     d = a & b
     return(d)
+
+def affair_meet(bob, alice, silvester):
+    a = set(alice)
+    b = set(bob)
+    c = set(silvester)
+    d = a & b
+    e = a & c
+    for i in e:
+        if i not in d:
+            f = i
+    return(f)
