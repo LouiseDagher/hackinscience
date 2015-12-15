22387c537df570d3adf3ea4fb7bd2db12c277c4b
Louise Dagher <louise.dagher@cri-paris.org>
2015-12-15 12:31:18 +0100

    essais 3

--- exercises/102/solution.py
+++ exercises/102/solution.py
@@ -1,7 +1,7 @@
 velib = [{
     'address': 'RUE DES CHAMPEAUX (PRES DE LA GARE ROUTIERE)\
     - 93170 BAGNOLET',
-    'zip': '93170-',
+    'zip': '93170',
     'number': 31705, 'latitude': 48.8645278209514, 'city': 'BAGNOLET',
     'name': 'CHAMPEAUX (BAGNOLET)-',
     'longitude': 2.416170724425901},
